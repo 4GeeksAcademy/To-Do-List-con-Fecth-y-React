9ae8bd075428b7d28f8720cf706d9d1ded85c7fb
Pablo Querales <pabloquerales20@gmail.com>
2024-12-16 13:59:41 +0100

    logica terminada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,54 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [listElement, setListElement] = useState([]);
+	const [inputChange, setInputChange] = useState("");
+
+	const handleChange = (e) => {
+		setInputChange(e.target.value);
+	};
+	const handleNewTask = (e) => {
+		if (e.key === "Enter" && e.target.value !== "") {
+			setListElement([...listElement, e.target.value]);
+			setInputChange("");
+			e.target.value = "";
+		}
+	};
+	const handleClick = (e) => {
+		const newLi = listElement.filter((element) => {
+			return e.target.parentElement.firstChild.textContent !== element;
+		});
+		setListElement(newLi);
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex flex-column align-items-center">
+			<h1>To-Do</h1>
+			<div className="input-group mb-3 w-50">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Username"
+					aria-label="Username"
+					aria-describedby="basic-addon1"
+					onKeyDown={handleNewTask}
+					onChange={handleChange}
+				/>
+			</div>
+			{listElement.map((element) => {
+				return (
+					<ul className="list-group w-50 d-flex justify-content-between">
+						<li className="list-group-item d-flex justify-content-between ">
+							{element}
+							<span onClick={handleClick}>x</span>
+						</li>
+					</ul>
+				);
+			})}
+			<div className="list-group w-50">
+				<span className="list-group-item">{listElement.length} CONTADOR</span>
+			</div>
 		</div>
 	);
 };
