2b2b5e254f1e1c8f8fb6170a9580312e7c69aae2
Pablo Querales <pabloquerales20@gmail.com>
2025-01-09 13:34:34 +0100

    se consumió la API para el metodo get y delet, falta anexar el metodo post para crear las tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,72 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
 	const [listElement, setListElement] = useState([]);
 
+	function getListElement() {
+		const requestOptions = {
+			method: "GET",
+			redirect: "follow"
+		};
+
+		fetch("https://playground.4geeks.com/todo/users/PabloQuerales", requestOptions)
+			.then((response) => response.json())
+			.then((result) => setListElement(result.todos))
+			.catch((error) => console.error(error));
+	}
+	function deletListElement(task) {
+		const requestOptions = {
+			method: "DELETE",
+			redirect: "follow"
+		};
+
+		fetch(`https://playground.4geeks.com/todo/todos/${task}`, requestOptions)
+			.then((response) => response.json())
+			.then((result) => console.log(result))
+			.catch((error) => console.error(error));
+	}
+	function createTask(task) {
+		const myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		const raw = JSON.stringify({
+			label: { task },
+			is_done: false
+		});
+
+		const requestOptions = {
+			method: "POST",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		fetch("https://playground.4geeks.com/todo/todos/PabloQuerales", requestOptions)
+			.then((response) => response.json())
+			.then((result) => console.log(result))
+			.catch((error) => console.error(error));
+	}
+	// REALIZA EL GET DE LA API AL INICIALIZAR LA APP
+	useEffect(() => {
+		getListElement();
+	}, []);
+
 	const handleNewTask = (e) => {
 		if (e.key === "Enter" && e.target.value !== "") {
 			setListElement([...listElement, e.target.value]);
+			createTask(e.target.value);
 			e.target.value = "";
 		}
 	};
+	console.log(listElement);
+
 	const handleClick = (e) => {
 		const newLi = listElement.filter((element) => {
 			return e.target.parentElement.firstChild.textContent !== element;
 		});
 		setListElement(newLi);
+		deletListElement(e.target.parentElement.id);
 	};
 	return (
 		<div className="container d-flex flex-column m-2 w-50 align-items-center p-5">
@@ -30,11 +81,11 @@ const Home = () => {
 					onKeyDown={handleNewTask}
 				/>
 			</div>
-			{listElement.map((element) => {
+			{listElement.map((task) => {
 				return (
 					<ul className="list-group w-50 d-flex justify-content-between">
-						<li className="list-group-item d-flex justify-content-between ">
-							{element}
+						<li className="list-group-item d-flex justify-content-between " key={task.id} id={task.id}>
+							{task.label}
 							<span className="button-delete" onClick={handleClick}>
 								x
 							</span>
